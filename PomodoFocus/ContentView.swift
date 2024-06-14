c3725750c6e4c40dc0237798ddcffd58096bbb93
Nikhil Barik <nklmarch17@gmail.com>
2024-06-14 08:57:38 +0530

    Initial Commit

---
+++ PomodoFocus/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  PomodoFocus
+//
+//  Created by Nikhil Barik on 14/06/24.
+//
+
+import SwiftUI
+import RealityKit
+import RealityKitContent
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Model3D(named: "Scene", bundle: realityKitContentBundle)
+                .padding(.bottom, 50)
+
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview(windowStyle: .automatic) {
+    ContentView()
+}
---
+++ PomodoFocus/PomodoFocusApp.swift
@@ -0,0 +1,17 @@
+//
+//  PomodoFocusApp.swift
+//  PomodoFocus
+//
+//  Created by Nikhil Barik on 14/06/24.
+//
+
+import SwiftUI
+
+@main
+struct PomodoFocusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
