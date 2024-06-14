349a8c4f8bc3276ae2b1a00bb7ce7659998373b2
Nikhil Barik <nklmarch17@gmail.com>
2024-06-14 12:01:35 +0530

    feat: [wip] sounds

--- PomodoFocus/SoundsView.swift
+++ PomodoFocus/SoundsView.swift
@@ -9,7 +9,44 @@ import SwiftUI
 
 struct SoundsView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 20) {
+            Text("Sounds on PomodoFocus is coming soon.")
+                .font(.largeTitle)
+                .multilineTextAlignment(.center)
+                .padding(.bottom, 40)
+            
+            Text("In the meantime, you can enjoy music on:")
+                .font(.title2)
+                .multilineTextAlignment(.center)
+                .padding(.bottom, 20)
+            
+            HStack(spacing: 20) {
+                Link(destination: URL(string: "https://www.spotify.com")!) {
+                    HStack {
+                        Image(systemName: "music.note")
+                        Text("Spotify")
+                    }
+                    .font(.title)
+                    .padding()
+                    .background(Color.gray.opacity(0.2))
+                    .foregroundColor(.primary)
+                    .cornerRadius(10)
+                }
+                
+                Link(destination: URL(string: "https://www.apple.com/apple-music/")!) {
+                    HStack {
+                        Image(systemName: "music.note")
+                        Text("Apple Music")
+                    }
+                    .font(.title)
+                    .padding()
+                    .background(Color.gray.opacity(0.2))
+                    .foregroundColor(.primary)
+                    .cornerRadius(10)
+                }
+            }
+        }
+        .padding()
     }
 }
 
