79012c9eb8ea3c32b9a7e635e070be6c83661e05
Nikhil Barik <nklmarch17@gmail.com>
2024-06-14 12:03:49 +0530

    fix: missed }

--- PomodoFocus/PomodoroView.swift
+++ PomodoFocus/PomodoroView.swift
@@ -87,6 +87,7 @@ struct PomodoroView: View {
         let minutes = Int(timeInterval) / 60
         let seconds = Int(timeInterval) % 60
         return String(format: "%02d:%02d", minutes, seconds)
+    }
 }
 
 #Preview {
